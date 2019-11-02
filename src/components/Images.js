80239242a4c6a9d4f106ad672b440998758a0316
Amy Hackett <hacketam@tcd.ie>
2019-11-02 21:56:27 +0000

    fixed photography images not appearing on github version

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,17 +1,17 @@
 
 
 export const  images = [
-    '/photography/CZAR3822.JPG',
-    '/photography/GJSJ2509.JPG',
-    '/photography/BIXE3594.JPG',
-    '/photography/VBTA1396.JPG',
+    './photography/CZAR3822.JPG',
+    './photography/GJSJ2509.JPG',
+    './photography/BIXE3594.JPG',
+    './photography/VBTA1396.JPG',
     
-    '/photography/DZVN7799.JPG',
-    '/photography/ETAZ7111.JPG',
-    '/photography/MUAD0763.JPG',
-    '/photography/GRBN3721.JPG',
-    '/photography/KGEU5313.JPG',
-    '/photography/LARB7509.JPG',
+    './photography/DZVN7799.JPG',
+    './photography/ETAZ7111.JPG',
+    './photography/MUAD0763.JPG',
+    './photography/GRBN3721.JPG',
+    './photography/KGEU5313.JPG',
+    './photography/LARB7509.JPG',
     
     
 ]
